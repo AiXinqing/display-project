e965fc3c5450b12575cfa9c8e9fe3870540d273a
qijiao <1003297709@qq.com>
2021-07-28 11:00:03 +0800

    引入vue-awesome-swiper

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import router from './router'
 import store from './store'
 // 将自动注册所有组件为全局组件
 import dataV from '@jiaminghi/data-view'
+import vueSwiper from 'vue-awesome-swiper' // 引入vue-awesome-swiper
+import 'swiper/dist/css/swiper.css' // 引入样式
 
 Vue.config.productionTip = false
 Vue.use(dataV)
+Vue.use(vueSwiper) // 使用插件
 new Vue({
   router,
   store,
