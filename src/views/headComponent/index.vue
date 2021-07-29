a1849c25a42699894721f115af9f781aae1029a0
qijiao <1003297709@qq.com>
2021-07-29 16:12:16 +0800

    head 组件

---
+++ src/views/headComponent/index.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="top-header">
+    <div class="top-header-box">
+      <div class="header-left">
+        <div class="scroll-tag" v-if="tagIs">
+          <span>正在播放</span>
+          <span>(<em>{{tagNum}}</em>/{{tagTotal}})</span>
+          <svg-oval class="svg-oval"/>
+          <svg-rectangle class="svg-rectangle"/>
+        </div>
+      </div>
+      <div class="header-center">{{title}}</div>
+      <div class="header-right">
+        <div v-html="timeDisplayArea" class="now_data" />
+      </div>
+    </div>
+    <heade-line/>
+  </div>
+</template>
+
+<script>
+import svgRectangle from '../svgIcon/rectangle'
+import svgOval from '../svgIcon/oval.vue'
+import headeLine from '../svgIcon/headeLine'
+export default {
+  components: {
+    svgRectangle,
+    svgOval,
+    headeLine
+  },
+  props: {
+    title: {
+      type: String,
+      default: '成都市第七中学大数据中心'
+    },
+
+    tagIs: {
+      type: Boolean,
+      default: true
+    },
+
+    tagNum: {
+      type: Number,
+      default: 1
+    },
+
+    tagTotal: {
+      type: Number,
+      default: 3
+    }
+  },
+
+  data () {
+    return {
+      nowDate: '', // 当前日期
+      currTime: new Date() // 当前日期对象
+    }
+  },
+
+  computed: {
+    timeDisplayArea () { // 时间展示区
+      const date = new Date()
+      const year = date.getFullYear() // 年
+      const month = date.getMonth() + 1 // 月
+      const day = date.getDate() // 日
+      const hours = this.currTime.getHours() > 9 ? this.currTime.getHours() : ('0' + this.currTime.getHours())
+      const minutes = this.currTime.getMinutes() > 9 ? this.currTime.getMinutes() : ('0' + this.currTime.getMinutes())
+      const seconds = this.currTime.getSeconds() > 9 ? this.currTime.getSeconds() : ('0' + this.currTime.getSeconds())
+      const week = date.getDay() // 星期
+      const weekArr = ['日', '一', '二', '三', '四', '五', '六']
+      return `<span>${year}</span>年<span>${month}</span>月<span>${day}</span>日<span>${hours}:</span><span>${minutes}:</span><span style="margin-right:5px">${seconds}</span>星期<span>${weekArr[week]}</span>`
+    }
+  },
+
+  created () {
+    this.currTime = new Date()
+    clearInterval(this.updateTimer)
+    this.updateTimer = setInterval(() => {
+      this.currTime = new Date()
+    }, 1000)
+  }
+}
+</script>
+
+<style lang="less">
+  @import '~@/assets/css/variables.less';
+  .top-header-box{
+    width: cacl(100% - 70px);
+    height: 84px;
+    display: flex;
+    line-height: 84px;
+    color: @white;
+    padding: 0 35px;
+
+    .header-left{
+      width: 30%;
+      font-size: 18px;
+      font-family: @Yahei;
+      color: #E9F1FC;
+      line-height: 24px;
+      letter-spacing: 1px;
+
+      em{
+        font-weight: bold;
+        color: @headeHighlight;
+      }
+
+      .svg-oval{
+        margin-left: 7px;
+        position: relative;
+        top: 4px;
+      }
+      .svg-rectangle{
+        margin-left: 7px;
+        position: relative;
+        top: 15px;
+      }
+    }
+    .header-center{
+      width: 40%;
+      text-align: center;
+      font-size: 40px;
+      font-family: @MicrosoftYaHeiBold;
+      font-weight: bold;
+      color: @headeC;
+    }
+    .header-right{
+      width: 30%;
+      text-align: right;
+      font-size: 24px;
+      font-family: @DINCondBlack;
+      font-weight: 900;
+
+      span{
+        color: @headeHighlight;
+      }
+    }
+  }
+</style>
