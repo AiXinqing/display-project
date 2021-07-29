e4cf5ebd5633f871c790def057294b3efefd3f86
qijiao <1003297709@qq.com>
2021-07-29 16:11:40 +0800

    添加heade 线性

---
+++ src/views/svgIcon/headeLine.vue
@@ -0,0 +1,37 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1906px" height="17px" viewBox="0 0 1906 17" version="1.1">
+    <title>路径</title>
+    <defs>
+        <linearGradient x1="50%" y1="-26.3744907%" x2="50%" y2="100%" id="linearGradient-1">
+            <stop stop-color="#122A47" offset="0%"/>
+            <stop stop-color="#102E52" offset="100%"/>
+        </linearGradient>
+        <linearGradient x1="0%" y1="50%" x2="99.2767866%" y2="49.9997596%" id="linearGradient-2">
+            <stop stop-color="#00FFFC" stop-opacity="0" offset="0%"/>
+            <stop stop-color="#00FFFC" offset="18.043352%"/>
+            <stop stop-color="#00FFFB" offset="24.5361374%"/>
+            <stop stop-color="#00FFFA" offset="34.1833568%"/>
+            <stop stop-color="#00FFFC" offset="53.1027692%"/>
+            <stop stop-color="#00FFFB" offset="62.6199514%"/>
+            <stop stop-color="#00FFFA" offset="77.931914%"/>
+            <stop stop-color="#00FFF9" offset="83.2226272%"/>
+            <stop stop-color="#00FFF8" stop-opacity="0" offset="100%"/>
+        </linearGradient>
+    </defs>
+    <g id="页面-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="1首页" transform="translate(0.000000, -64.000000)" fill="url(#linearGradient-1)" stroke="url(#linearGradient-2)">
+            <polyline id="路径" points="-0.264438031 65.2233836 631.060408 65.2233836 661.949662 79.5416337 1258.71945 79.5416337 1290.17829 65.2233836 1919.5 65.2233836"/>
+        </g>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
